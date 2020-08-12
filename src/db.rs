997f5c93d2da1bc2bae4a4a94e6f4dc47217d1a8
nasko25 <apashov0@gmail.com>
2020-08-12 20:12:23 +0300

    connecting to database and added a possible schema

--- src/db.rs
+++ src/db.rs
@@ -1,16 +1,59 @@
 // TODO tables https://docs.rs/diesel/1.4.5/diesel/macro.table.html
 // https://github.com/diesel-rs/diesel/blob/master/examples/mysql/getting_started_step_1/src/schema.rs
+use diesel::prelude::*;
+#[cfg(test)]
+use diesel::mysql::MysqlConnection;
 
-struct Database {
+mod schema {
+    diesel::table! {
+        website (id) {
+            id -> Integer,
+            title -> Text,
+            metadata -> Text,
+            url -> Varchar,
+            rank -> Integer,
+            type_of_website -> Varchar,             // TODO table for website types?
+        }
+    }
+    diesel::table! {
+        keywords (website_id, keyword) {
+            website_id -> Integer,
+            keyword -> Varchar,
+            rank_per_kw -> Varchar,
+        }
+    }
+}
+
+// use schema::website;
+
+#[derive(Queryable)]
+pub struct Website {
+    pub id: i32,
+    pub title: String,
+    pub metadata: String,
+    pub url: String,
+    pub rank: i32,
+    pub type_of_website: String
+}
+
+#[derive(Queryable)]
+pub struct Keywords {
+    pub website_id: i32,
+    pub keyword: String,
+    pub rank_per_kw: String
+}
+
+pub struct Database {
     server: String,
     port: u16
 }
 
 impl Database {
-    pub fn init() {
-
+    pub fn establish_connection(db_url: &str) -> MysqlConnection {
+        MysqlConnection::establish(&db_url)
+            .unwrap_or_else(|_| panic!("Error connecting to {}", db_url))
     }
-    pub fn conn() {
-        // TODO
-    }
+    // pub fn conn() {
+    //     // TODO
+    // }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 extern crate config;
 extern crate serde;
-#[macro_use]
-extern crate serde_derive;
+#[macro_use] extern crate serde_derive;
+#[macro_use] extern crate diesel;
 
 mod settings;
+mod db;
 
 fn main() {
     let settings = settings::Settings::new(false);
     println!("{:?}", settings);
     println!("{:?}", settings.unwrap().get_serv());
+
+    // TODO get url from config
+    db::Database::establish_connection(&"mysql://asdf:asdf@localhost:3306/querty");
 }
