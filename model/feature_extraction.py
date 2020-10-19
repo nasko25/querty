00955e22c0e53dd3efb228f864f272b78280d81e
nasko25 <apashov0@gmail.com>
2020-10-19 11:09:58 +0300

    added a folder that will be used to train a model to classify websites by genres

---
+++ model/feature_extraction.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# These are the categories in the KI-04 dataset.
+# The websites are sorted by their genre. Each type of website is put in a folder, the names of the folders are:
+genres = {
+    "articles",
+    "discussion",
+    "download",
+    "help",
+    "linklists",
+    "portrait-non_priv",
+    "portrait-priv",
+    "shop"
+}
