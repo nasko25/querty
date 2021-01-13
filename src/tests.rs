8e9515206e209e989c38a289d5a3321498fe6dca
nasko25 <apashov0@gmail.com>
2021-01-13 12:03:49 +0200

    fixed the tests
    added the base_url to the initilization of the Website struct, and fixed some testing errors that happened because solr and the db were reset

--- src/tests.rs
+++ src/tests.rs
@@ -25,7 +25,7 @@ pub fn test_all(settings: &settings::Settings, conn: &MysqlConnection) -> Result
     let creat_website = db::Database::create_tables(conn);
     println!("table website created: {:?}", creat_website);
 
-    let w = DB::Website(Website { id: None, title: "".to_string(), text: "This is a website for some things".to_string(), url: "".to_string(), rank: 3.0, type_of_website: "".to_string() });
+    let w = DB::Website(Website { id: None, title: "".to_string(), text: "This is a website for some things".to_string(), url: "".to_string(), base_url: "".to_string(), rank: 3.0, type_of_website: "".to_string() });
     // let mut vals_inserted = db::Database::insert_w(&w, conn);
     // println!("values inserted: {:?}", vals_inserted);
 
@@ -39,7 +39,7 @@ pub fn test_all(settings: &settings::Settings, conn: &MysqlConnection) -> Result
         println!("Inserted website: {:?}", website);
         website.rank = 7.0;
         println!("Website rank should be updated: {:?}", db::Database::update(&DB::Website(website.clone()), conn));
-        let website_solr = WebsiteSolr {id: website.id, title: website.title, text: website.text, url: website.url, rank: website.rank, type_of_website: website.type_of_website, metadata: None, external_links: None};
+        let website_solr = WebsiteSolr {id: website.id, title: website.title, text: website.text, url: website.url, base_url: website.base_url, rank: website.rank, type_of_website: website.type_of_website, metadata: None, external_links: None};
         println!("Inserted in Solr: {:?}", insert(settings, &website_solr));
     }
     // println!("{:?}", db::Database::insert(&u, conn));
@@ -48,7 +48,7 @@ pub fn test_all(settings: &settings::Settings, conn: &MysqlConnection) -> Result
     println!("{:?}", req(settings, "*:*".to_string()));
 
     // let mut website_ids = crate::schema::website::dsl::website.filter(crate::schema::website::dsl::id.eq(110)).load::<Website>(conn).expect("Error loading website");
-    let mut website_ids = db::Database::select_w(&Some(vec![ 110 ]), conn);
+    let mut website_ids = db::Database::select_w(&Some(vec![ 2 ]), conn);
     let md = db::Database::select_m(&Some(website_ids.clone()), conn);
     println!("Metadata: {:?}", &md);
 
@@ -58,7 +58,7 @@ pub fn test_all(settings: &settings::Settings, conn: &MysqlConnection) -> Result
     let mut website_solr = website_solr_vec.get(0).unwrap();
     println!("\n\nUpdate metadata: {:?}", update_metadata(settings, &md, &website_solr));
 
-    website_ids = db::Database::select_w(&Some(vec![ 109 ]), conn);
+    website_ids = db::Database::select_w(&Some(vec![ 1 ]), conn);
 
     let ext_links = db::Database::select_el(&website_ids.get(0), conn);
     println!("External Links: {:?}", ext_links);
@@ -71,13 +71,13 @@ pub fn test_all(settings: &settings::Settings, conn: &MysqlConnection) -> Result
     let m = DB::Metadata(Metadata {id: None, metadata_text: "some metadata text".to_string(), website_id: website_ids.get(0).unwrap().id});
     println!("Metadata should be inserted: {:?}", db::Database::insert(&m, conn));
 
-    let m_err = DB::Metadata(Metadata {id: None, metadata_text: "some metadata text".to_string(), website_id: Some(9)});
+    let m_err = DB::Metadata(Metadata {id: None, metadata_text: "some metadata text".to_string(), website_id: Some(200)});
     println!("Metadata insert should trow a foreign key violation: {:?}", db::Database::insert(&m_err, conn));
 
     let e_l = DB::ExternalLink(ExternalLink {id: None, url: "http://example.com/asdf/@usr/$".to_string()});
     println!("External Link should be inserted: {:?}", db::Database::insert(&e_l, conn));
 
-    let w_r_e_l = DB::WebsiteRefExtLink(WebsiteRefExtLink {id: None, website_id: Some(110), ext_link_id: Some(2)});
+    let w_r_e_l = DB::WebsiteRefExtLink(WebsiteRefExtLink {id: None, website_id: Some(2), ext_link_id: Some(2)});
     println!("Website reference external link should be inserted: {:?}", db::Database::insert(&w_r_e_l, conn));
 
     let w_r_e_l_err = DB::WebsiteRefExtLink(WebsiteRefExtLink {id: None, website_id: Some(200), ext_link_id: Some(300)});
