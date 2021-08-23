b65185955d8d2a255669dafc76c62b8ff55f710d
nasko25 <apashov0@gmail.com>
2021-08-23 18:17:01 +0300

    uncommented the crawler and user_react tests

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ extern crate config;
 extern crate serde;
 #[macro_use] extern crate serde_derive;
 #[macro_use] extern crate diesel;
-//#[macro_use] extern crate throw;
-//#[macro_use] extern crate simple_error;
+#[macro_use] extern crate throw;
+#[macro_use] extern crate simple_error;
 
 // used for colorful output
 extern crate colour;
@@ -20,8 +20,8 @@ mod settings;
 mod schema;
 mod db;
 mod solr;
-//mod tests;
-//mod crawler;
+mod tests;
+mod crawler;
 mod web_api;
 
 //use tests::test_all;
@@ -65,17 +65,17 @@ fn init(settings: settings::Settings) {
     println!("web saved: {:?}", websites_saved);
 
     // analyse a website and update its rank
-    //let crawler = crawler::Crawler {
-    //    conn: &conn,
-    //    settings: &settings
-    //};
-    //crawler.analyse_website(&url, &websites_saved).unwrap();
-    //let updated_rank = user_react(url, React::Upvote { val: 0.0 }, &settings, &conn);
+    let crawler = crawler::Crawler {
+        conn: &conn,
+        settings: &settings
+    };
+    crawler.analyse_website(&url, &websites_saved).unwrap();
+    let updated_rank = user_react(url, React::Upvote { val: 0.0 }, &settings, &conn);
 
-    //match updated_rank {
-    //    Ok(new_rank) => println!("Rank updated successfully. New rank: {}", new_rank),
-    //    Err(err) => println!("Rank was not updated successfully: Err({})", err),
-    //}
+    match updated_rank {
+        Ok(new_rank) => println!("Rank updated successfully. New rank: {}", new_rank),
+        Err(err) => println!("Rank was not updated successfully: Err({})", err),
+    }
 }
 
 fn main() {
@@ -96,69 +96,68 @@ fn main() {
 // later this could depend on user's ranks
 // TODO more sensible name than "val"
 // #[derive(PartialEq)]
-//enum React {
-//    Upvote { val: f64 },
-//    Downvote { val: f64 },
-//}
-//
-//enum ReactError {
-//    InvalidArgument { mes: String },
-//    RankNotUpdated { mes: String },
-//    GenericError
-//}
-//
-//impl fmt::Display for ReactError {
-//    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-//        match self {
-//            ReactError::InvalidArgument { mes } => write!(f, "{}", mes),
-//            ReactError::RankNotUpdated { mes } => write!(f, "{}", mes),
-//            ReactError::GenericError => write!(f, "An error occured in user_react()") // TODO more sensible error message
-//        }
-//    }
-//}
-//// TODO passing settings and MysqlConnection everywhere is probably not a good idea
-//// refactor?
-//fn user_react(url: &str, react_type: React, settings: &settings::Settings, conn: &MysqlConnection) -> Result<f64, ReactError> {
-//    println!("Updating the website with url {} after user react.", url);
-//    let mut websites_saved = solr::req(&settings, format!("url:\"{}\"", url)).unwrap();
-//    // websites_saved should either be empty (if there are no websites with that url in solr)
-//    //      in which case the website should just be analysed and its rank should be set to 0.0
-//    //
-//    // or websites_saved should have a length of 1 (because olny 1 website should have been fetched from solr
-//    // because url should be unique)
-//    if websites_saved.is_empty() {}
-//    // since website ranks should be between -10 and 10 and user react FOR NOW will only update it
-//    // with +/-1, I can do this ugly check
-//    else if websites_saved.len() == 1 && ((websites_saved[0].rank <= 9.0 && discriminant(&react_type) == discriminant(&React::Upvote{ val: 0.0 })) || (websites_saved[0].rank >= -9.0 && discriminant(&react_type) == discriminant(&React::Downvote {val: 0.0}))) {
-//        println!("{:?}'s old rank: {}", websites_saved[0].id, websites_saved[0].rank);
-//        websites_saved[0].rank += match react_type {
-//            React::Upvote { val } => {
-//                println!("Upvote val: {}", val);
-//                1.0
-//            },
-//            React::Downvote { val } => {
-//                println!("Downvote val: {}", val);
-//                -1.0
-//            },
-//        };
-//    }
-//    else if websites_saved.len() != 1 {
-//        return Err(ReactError::InvalidArgument { mes: "Vector is not empty and has a size != 1.".to_string() });
-//    }
-//
-//    else {
-//        return Err(ReactError::GenericError);
-//    }
-//    let crawler = crawler::Crawler {
-//        conn,
-//        settings
-//    };
-//    crawler.analyse_website(&url, &websites_saved).unwrap();
-//
-//    if websites_saved.is_empty() {
-//        return Err(ReactError::RankNotUpdated { mes: "Url has not been analysed previously, so its rank was set to 0.".to_string() });
-//    }
-//
-//    Ok(websites_saved[0].rank)
-//}
-//
+enum React {
+    Upvote { val: f64 },
+    Downvote { val: f64 },
+}
+
+enum ReactError {
+    InvalidArgument { mes: String },
+    RankNotUpdated { mes: String },
+    GenericError
+}
+
+impl fmt::Display for ReactError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ReactError::InvalidArgument { mes } => write!(f, "{}", mes),
+            ReactError::RankNotUpdated { mes } => write!(f, "{}", mes),
+            ReactError::GenericError => write!(f, "An error occured in user_react()") // TODO more sensible error message
+        }
+    }
+}
+// TODO passing settings and MysqlConnection everywhere is probably not a good idea
+// refactor?
+fn user_react(url: &str, react_type: React, settings: &settings::Settings, conn: &MysqlConnection) -> Result<f64, ReactError> {
+    println!("Updating the website with url {} after user react.", url);
+    let mut websites_saved = solr::req(&settings, format!("url:\"{}\"", url)).unwrap();
+    // websites_saved should either be empty (if there are no websites with that url in solr)
+    //      in which case the website should just be analysed and its rank should be set to 0.0
+    //
+    // or websites_saved should have a length of 1 (because olny 1 website should have been fetched from solr
+    // because url should be unique)
+    if websites_saved.is_empty() {}
+    // since website ranks should be between -10 and 10 and user react FOR NOW will only update it
+    // with +/-1, I can do this ugly check
+    else if websites_saved.len() == 1 && ((websites_saved[0].rank <= 9.0 && discriminant(&react_type) == discriminant(&React::Upvote{ val: 0.0 })) || (websites_saved[0].rank >= -9.0 && discriminant(&react_type) == discriminant(&React::Downvote {val: 0.0}))) {
+        println!("{:?}'s old rank: {}", websites_saved[0].id, websites_saved[0].rank);
+        websites_saved[0].rank += match react_type {
+            React::Upvote { val } => {
+                println!("Upvote val: {}", val);
+                1.0
+            },
+            React::Downvote { val } => {
+                println!("Downvote val: {}", val);
+                -1.0
+            },
+        };
+    }
+    else if websites_saved.len() != 1 {
+        return Err(ReactError::InvalidArgument { mes: "Vector is not empty and has a size != 1.".to_string() });
+    }
+
+    else {
+        return Err(ReactError::GenericError);
+    }
+    let crawler = crawler::Crawler {
+        conn,
+        settings
+    };
+    crawler.analyse_website(&url, &websites_saved).unwrap();
+
+    if websites_saved.is_empty() {
+        return Err(ReactError::RankNotUpdated { mes: "Url has not been analysed previously, so its rank was set to 0.".to_string() });
+    }
+
+    Ok(websites_saved[0].rank)
+}
