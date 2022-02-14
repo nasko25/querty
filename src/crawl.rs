f988b8841f5b0a8d60b98967543d1b36d72a41c5
nasko25 <apashov0@gmail.com>
2022-02-14 14:08:30 +0200

    fixed warnings from the crawl macro
    break the loop if there are no new urls that need to be crawled next

--- src/crawl.rs
+++ src/crawl.rs
@@ -1,23 +1,28 @@
+// TODO make function again instead of macro
 // use the crawler and the next_urls_to_crawl db table to crawl continuously
 #[macro_export]
 macro_rules! crawl {
     () => {
         async_std::task::spawn(async {
-            let mut next_url: NextUrl;
             loop {
-                next_url = match db::Database::select_next_crawl_url() {
+                let next_url = match db::Database::select_next_crawl_url() {
                     Ok(url) => url,
+                    // break if there are no more urls to be crawled
+                    Err(diesel::result::Error::NotFound) => break,
                     Err(err) => return Err(err),
                 };
                 // TODO ...
                 // also add urls that have not yet been crawled linked from that url
 
                 // delete the url after crawling it
-                db::Database::delete_crawled_url(next_url.url);
+                match db::Database::delete_crawled_url(next_url.url) {
+                    Ok(_) => (),
+                    Err(err) => return Err(err),
+                }
                 async_std::task::sleep(std::time::Duration::from_secs(20)).await;
                 println!("Crawl running...");
             }
-            Ok(next_url)
+            Ok(())
         })
     };
 }
